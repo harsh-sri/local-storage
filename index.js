b462e6fd9d4dac76737a90d3a37d1d598760c909
Harsh <harsh.srivastava51@gmail.com>
2019-05-14 13:45:07 +0530

    added a comment

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 exports.localStorage = {
-
+    /**
+     * Desc: Save object into Local Storage
+     * @param {*} key : key name for object
+     * @param {*} value :  object
+     */
     setObj: function (key, value) {
         try {
             if(window && window.localStorage) {
@@ -11,6 +15,10 @@ exports.localStorage = {
         }
     },
 
+    /**
+     * Desc: Get the object from local storage 
+     * @param {*} key : Pass the keyname 
+     */
     getObj: function (key) {
         try {
             if(window && window.localStorage) {
@@ -23,6 +31,11 @@ exports.localStorage = {
         }
     },
 
+    /**
+     * Desc: To save values into Local Storage
+     * @param {*} key : name of the key
+     * @param {*} value : value of the key
+     */
     set: function (key, value) {
         try {
             if(window && window.localStorage) {
@@ -34,6 +47,11 @@ exports.localStorage = {
         }
     },
 
+    /**
+     * Desc: Get the value from Local Storage
+     * @param {*} key : Name of the key
+     * @param {*} value : value of the key
+     */
     get: function (key, value) {
         try {
             if(window && window.localStorage) {
@@ -45,6 +63,10 @@ exports.localStorage = {
         }
     },
 
+    /**
+     * Desc: Remove the specific key and associated value from local storage
+     * @param {*} key : name of the key to be removed
+     */
     remove: function (key) {
         try {
             if(window && window.localStorage) {
@@ -56,6 +78,9 @@ exports.localStorage = {
         }
     },
 
+    /**
+     * Desc: This function will remove the entire Local Storage
+     */
     clear: function () {
         try {
             if(window && window.localStorage) {
