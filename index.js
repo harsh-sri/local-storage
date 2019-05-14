63d876fe2ce697b853a236f9db8b57133be54ad4
Harsh <harsh.srivastava51@gmail.com>
2019-05-14 13:22:13 +0530

    added a setObj function to save object into localstorage

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 exports.localStorage = {
 
-
+    setObj: function (key, value) {
+        try {
+            if(window && window.localStorage) {
+                return window.localStorage.setItem(key, JSON.stringify(value));
+            }
+        }
+        catch(e) {
+            console.error(e);
+        }
+    }
 
 }
